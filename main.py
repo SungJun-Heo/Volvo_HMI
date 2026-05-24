527aa235d96c87ed60ee62d34395632525298852
HSJ <timetravel1016@gmail.com>
2026-05-24 10:41:51 +0900

    debug: add __init__.py

--- main.py
+++ main.py
@@ -1,15 +1,43 @@
 import sys
-from src import SharedMemory, TCPCommunicator, LLMProcessor, OutputProcessor
-from src.STTProcessor import STTProcessor
+import argparse
+
+from src.SharedMemory import SharedMemory
+from src.OutputProcessor import OutputProcessor
+from src.TCPCommunicator import TCPCommunicator
 from src.GUIApp import launch_gui
 
+
+def parse_args():
+    p = argparse.ArgumentParser(description="Volvo HMI")
+    p.add_argument("--dummy-stt",    action="store_true",    help="STTProcessor 대신 DummySTTProcessor 사용")
+    p.add_argument("--dummy-llm",    action="store_true",    help="LLMProcessor 대신 DummyLLMProcessor 사용")
+    p.add_argument("--tcp-address",  default="127.0.0.1:8888", metavar="HOST:PORT")
+    return p.parse_args()
+
+
 if __name__ == "__main__":
+    args = parse_args()
+
+    tcp_host, tcp_port = args.tcp_address.rsplit(":", 1)
+
     shm = SharedMemory()
 
-    tcp    = TCPCommunicator(shm)
-    llm    = LLMProcessor(shm)
+    if args.dummy_stt:
+        from test.dummy_stt import DummySTTProcessor
+        stt = DummySTTProcessor(shm)
+    else:
+        from src.STTProcessor import STTProcessor
+        stt = STTProcessor(shm)
+
+    if args.dummy_llm:
+        from test.dummy_llm import DummyLLMProcessor
+        llm = DummyLLMProcessor(shm)
+    else:
+        from src.LLMProcessor import LLMProcessor
+        llm = LLMProcessor(shm)
+
+    tcp    = TCPCommunicator(shm, ip=tcp_host, port=int(tcp_port))
     output = OutputProcessor(shm)
-    stt    = STTProcessor(shm)
 
     tcp.start()
     llm.start()
